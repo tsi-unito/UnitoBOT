5348bcc1cb5e67172766221fc97599eb0a8d574a
Stefano Vittorio Porta <stefa168@hotmail.it>
2023-10-04 15:49:56 +0200

    fix: fixed text style not working well in markdown

--- bot.py
+++ bot.py
@@ -31,18 +31,38 @@ async def link_gruppi(update: Update, _):
             command = entities.get(key).strip("/")
             break
 
-    links = {
-        "ot": "https://t.me/+_zMDhpzM3q1iNjE0",
-        "generale": "https://t.me/joinchat/Ci07EELN-R3W2xI6-SGfGg",
-        "magistrale": "https://t.me/joinchat/BbqyERQcACYhQFEO1iJD2g",
-        "matricole": "https://t.me/+Ox2fUmU2Un4xYTM0",
-        "anno1": "https://t.me/+Ox2fUmU2Un4xYTM0",
-        "anno2": "https://t.me/joinchat/huoxYswWOLQ5Mjk0",
-        "anno3": "https://t.me/joinchat/UmWgshpk8MXD_Y4KvLyU8A",
-        "links": "https://tsi-unito.eu/links",
-    }
+    class ResourceData:
+        shortcut: list[str] | None
+        name: str
+        url: str | list[str]
+
+        def __init__(self, shortcut: list[str] | None, name: str, url: str | list[str]):
+            self.shortcut = shortcut
+            self.name = name
+            self.url = url
+
+    links: dict[str, ResourceData] = {}
+
+    _initialization_link_list = [
+        ResourceData(["ot"], "Gruppo Off-Topic", "https://t.me/+_zMDhpzM3q1iNjE0"),
+        ResourceData(["generale"], "Gruppo Generale", "https://t.me/joinchat/Ci07EELN-R3W2xI6-SGfGg"),
+        ResourceData(["magistrale"], "Gruppo della Magistrale", "https://t.me/joinchat/BbqyERQcACYhQFEO1iJD2g"),
+        ResourceData(["anno1", "matricole"], "Gruppo delle Matricole (Primo Anno)", "https://t.me/+Ox2fUmU2Un4xYTM0"),
+        ResourceData(["anno2"], "Gruppo per gli Studenti del Secondo Anno", "https://t.me/joinchat/huoxYswWOLQ5Mjk0"),
+        ResourceData(["anno3"], "Gruppo per gli Studenti del Terzo Anno",
+                     "https://t.me/joinchat/UmWgshpk8MXD_Y4KvLyU8A"),
+        ResourceData(["links"], "Lista dei link", "https://tsi-unito.eu/links"),
+    ]
+
+    # Add every shortcut to the dictionary. If there are multiple shortcuts, add each of them as a new entry
+    for resource in _initialization_link_list:
+        for shortcut in resource.shortcut:
+            links[shortcut] = resource
 
     link = links.get(command)
+    if link is None:
+        print(f"Command {command} not found")
+        return
     # Got the correct link. Now let's see how to behave...
 
     message_in_group = message.chat_id != message.from_user.id
@@ -60,18 +80,21 @@ async def link_gruppi(update: Update, _):
         if is_replying:
             # The id of the message we should reply to, and NOT the invoker of the command
             original_message = reply_message.message_id
-            await message.reply_text(f"[Ecco il link]({link})\\!",
-                                     parse_mode=ParseMode.MARKDOWN_V2,
+            await message.reply_text(f"<b>Ecco il link che hai richiesto:</b>\n » <a href='{link.url}'>{link.name}</a>",
+                                     parse_mode=ParseMode.HTML,
                                      reply_to_message_id=original_message)
         else:
             user_name = message.from_user.full_name
             user_id = message.from_user.id
-            await message.reply_text(f"[Ecco qua il link]({link}) [{user_name}](tg://user?id={user_id})\\!",
-                                     quote=False,
-                                     message_thread_id=message_thread,
-                                     parse_mode=ParseMode.MARKDOWN_V2)
+            await message.reply_text(
+                f"<b>Ecco il link che hai richiesto</b> <a href='tg://user?id={user_id}'>{user_name}</a>:\n » <a href='{link.url}'>{link.name}</a>",
+                quote=False,
+                message_thread_id=message_thread,
+                parse_mode=ParseMode.HTML
+            )
     else:
-        await message.reply_text(f"[Ecco il link]({link})\\!", parse_mode=ParseMode.MARKDOWN_V2)
+        await message.reply_text(f"<b>Ecco il link:</b>\n<a href='{link.url}'>{link.name}</a>",
+                                 parse_mode=ParseMode.HTML)
 
     try:
         await message.delete()
