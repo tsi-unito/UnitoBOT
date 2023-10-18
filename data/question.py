8b35c9f78960227f0b1c4cc0e2f5cb0a8af5d778
Stefano Vittorio Porta <stefa168@hotmail.it>
2023-10-19 01:19:32 +0200

    refactor: rewritten class to use declarative ORM

--- data/question.py
+++ data/question.py
@@ -1,36 +1,38 @@
 from dataclasses import dataclass
 from datetime import datetime
 
-from sqlalchemy import Table, Column, Integer, BigInteger, String, DateTime, func
-from sqlalchemy.orm import registry
-
-mapper_registry = registry()
+import sqlalchemy as sa
+from sqlalchemy.orm import Mapped, DeclarativeBase, mapped_column
 
 
-@mapper_registry.mapped
+class SQLAlchemyBase(DeclarativeBase):
+    pass
+
+
 @dataclass
-class Question:
-    _tablename = "nlp_questions"
-    __table__ = Table(
-        _tablename,
-        mapper_registry.metadata,
-        Column("id", Integer(), primary_key=True, nullable=False),
-        Column("message_id", BigInteger(), index=True, unique=True, nullable=False),
-        Column("user_id", BigInteger(), index=True, unique=True, nullable=False),
-        Column("message", String(), nullable=False),
-        Column("date", DateTime(), server_default=func.now(), nullable=False)
-    )
+class Question(SQLAlchemyBase):
+    __tablename__ = "nlp_questions"
 
-    id: int
-    message_id: int
-    user_id: int
-    message: str
-    date: datetime
+    id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
+    message_id: Mapped[int] = mapped_column(sa.BigInteger, nullable=False, index=True)
+    user_id: Mapped[int] = mapped_column(sa.BigInteger, nullable=False, index=True)
+    message: Mapped[str] = mapped_column(nullable=False)
+    date: Mapped[datetime] = mapped_column(server_default=sa.func.now(), nullable=False)
 
     def __init__(self, message_id: int, user_id: int, message: str):
+        # noinspection PyTypeChecker
         self.message_id = message_id
+        # noinspection PyTypeChecker
         self.user_id = user_id
+        # noinspection PyTypeChecker
         self.message = message
+        super().__init__()
 
     def __repr__(self):
         return f"<Question(id={self.id}, message_id={self.message_id}, user_id={self.user_id}, message={self.message}, date={self.date})>"
+
+# class Feedback(DeclarativeBase):
+#     __tablename__ = "feedbacks"
+#
+#     id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
+#     question_id: Mapped
